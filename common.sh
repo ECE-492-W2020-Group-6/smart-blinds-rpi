5f7ec07a3f97fe270eb4b757e7bde29ced78829e
Ishaat Chowdhury <ishaat@ualberta.ca>
2020-02-08 15:53:40 -0700

    Update common.sh

--- common.sh
+++ common.sh
@@ -16,9 +16,14 @@ if ! docker buildx > /dev/null 2>&1; then
     mkdir -p ~/.docker/cli-plugins/
     wget -O ~/.docker/cli-plugins/docker-buildx  https://github.com/docker/buildx/releases/download/v0.3.1/buildx-v0.3.1.linux-amd64
     chmod a+x ~/.docker/cli-plugins/docker-buildx
-    
+
+    export DOCKER_CLI_EXPERIMENTAL=enabled   
+
+    sudo systemctl restart docker
+
     if ! docker buildx > /dev/null 2>&1; then
         echo "docker buildx installation failed"
+        docker info
         exit 1
     fi
 fi
