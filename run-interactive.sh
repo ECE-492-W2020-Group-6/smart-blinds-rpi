27f02d6b5c0424687b3a123f0841c18b273e803a
Ishaat Chowdhury <ishaat@ualberta.ca>
2020-02-08 13:41:58 -0700

    Add script to start interactive docker container

---
+++ run-interactive.sh
@@ -0,0 +1,21 @@
+# Date: Feb 8, 2020
+# Author: Ishaat Chowdhury
+# Contents: bash script to run interactive process for docker image
+
+#!/bin/bash
+
+source common.sh
+
+# Run image in daemon mode
+docker run -it -d --name rpi-code $RPI_IMAGE
+
+# Execute shell in container
+docker exec -it rpi-code /bin/bash
+
+# Cleanup container
+docker stop rpi-code
+
+docker rm rpi-code
+
+
+
